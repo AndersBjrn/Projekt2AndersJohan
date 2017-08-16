82c0cf6725931eaea1bd99f3ad2549bbbdad7310
STENUNGSUND\anders.bjorn <anders.bjorn@outlook.com>
2017-08-16 16:13:15 +0200

    Added methods to create ships and ask for user input

--- GroupWork2/GroupWork2/Program.cs
+++ GroupWork2/GroupWork2/Program.cs
@@ -10,6 +10,62 @@ namespace GroupWork2
     {
         static void Main(string[] args)
         {
+            GameBoard board = CreateBoard();
+            ChooseShips(board);
+            PlaceShips();
+        }
+
+        private static List<Ship> ChooseShips(GameBoard board)
+        {
+            Console.WriteLine("Do you want default number of ships?(y/n):");
+            string answer = YesOrNo();
+            if (answer == "y")
+            {
+                return DecideDefaultNumberOfShips();
+            }
+            else
+            {
+                List<Ship> ships = new List<Ship>();
+                Console.Write("How many Battleships(size 4)?: ");
+                int battleships = CheckThatInputIsInt();
+                return ships;
+            }
+        }
+
+        private static string YesOrNo()//Checks that the answer is yes, y or no, n and returns y or n in lower case
+        {
+            string input = Console.ReadLine();
+            while (!input.Equals("yes", StringComparison.InvariantCultureIgnoreCase) && !input.Equals("no", StringComparison.InvariantCultureIgnoreCase) && !input.Equals("y",StringComparison.InvariantCultureIgnoreCase) && !input.Equals("n", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("Enter answer again (y/n): ");
+                input = Console.ReadLine();
+            }
+            if (input.Equals("yes", StringComparison.InvariantCultureIgnoreCase) || input.Equals("y", StringComparison.InvariantCultureIgnoreCase))
+            {
+                input = "y";
+            }
+            else
+            {
+                input = "n";
+            }
+            return input;
+        }
+
+        private static int CheckThatInputIsInt()//Makes sure the imput string is convertable to int and returns it
+        {
+            int inputInt = 0;
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out inputInt))
+            {
+                Console.Write("Please enter a number again: ");
+                input = Console.ReadLine();
+            }
+            return inputInt;
+        }
+
+        private static void PlaceShips(board)
+        {
+            Console.WriteLine();
         }
     }
 }
---
+++ GroupWork2/GroupWork2/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWork2
+{
+    class Ship
+    {
+        string name;
+        int size;
+
+        public Ship(int size)
+        {
+            this.size = size;
+            switch (size)
+            {
+                case 1:
+                    name = "Submarine";
+                    break;
+                case 2:
+                    name = "Destroyer";
+                    break;
+                case 3:
+                    name = "Cruiser";
+                    break;
+                case 4:
+                    name = "Battleship";
+                    break;
+            }
+        }
+    }
+}
